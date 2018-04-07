adfb03ba9b04477aecbc57d93eb3371f55288051
Andres Rios <rianby64@gmail.com>
2018-04-07 07:22:20 +0300

    Organice un poco la estructura de datos

--- static/report.js
+++ static/report.js
@@ -1,32 +1,47 @@
 
 (() => {
+  var DTSym = Symbol();
   var supplies = [];
+  var periods = [];
   window.supplies = supplies;
+  window.periods = periods;
 
   function Report() {
 
   }
 
   Report.prototype.render = function render(row) {
-    console.log(row);
+    var found;
+    var period = {
+      start: row.start,
+      end: row.end
+    };
+    period[DTSym] = `${row.start.toISOString()}-${row.end.toISOString()}`;
+    found = periods.find(d => d[DTSym] == period[DTSym]);
+    if (!found) {
+      periods.push(period);
+    }
+    var APU = {
+      APUId:              row.APUId,
+      APU_description:    row.APU_description,
+      APU_is_estimated:   row.APU_is_estimated,
+      APU_unit:           row.APU_unit,
+      Tasks_start:        row.Tasks_start,
+      Tasks_end:          row.Tasks_end,
+      periods: [period]
+    };
     var supply = {
       SupplyId:             row.SupplyId,
       Supplies_cost:        row.Supplies_cost,
       Supplies_description: row.Supplies_description,
       Supplies_type:        row.Supplies_type,
       Supplies_unit:        row.Supplies_unit,
-      APU: [{
-        APUId:              row.APUId,
-        APU_description:    row.APU_description,
-        APU_is_estimated:   row.APU_is_estimated,
-        APU_unit:           row.APU_unit
-        Tasks_start:        row.Tasks_start,
-        Tasks_end:          row.Tasks_end
-      }]
+      APU: [APU]
     };
-    var found = supplies.find(d => d.SupplyId == supply.SupplyId);
+
+    found = supplies.find(d => d.SupplyId == supply.SupplyId);
     if (found) {
-      found.APU.push(supply.APU[0]);
+      found.APU.push(APU);
     } else {
       supplies.push(supply);
     }
