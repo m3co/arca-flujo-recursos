50b963af97f8db3a45ec98fc563d2cf96dbad69c
Andres Rios <rianby64@gmail.com>
2018-09-02 11:20:10 +0200

    Defina el combobox para seleccionar proyecto

--- static/client.js
+++ static/client.js
@@ -1,26 +1,38 @@
 'use strict';
 ((io) => {
   var client = io();
+  var ProjectId = location.search.match(/\d+$/);
   client.on('connect', () => {
     console.log('connection');
+    if (ProjectId) {
+      client.emit('data', {
+        query: 'select',
+        module: 'viewAAUSuppliesCosts1MonthFlow',
+        project: '8'
+      });
+    }
 
     client.emit('data', {
       query: 'select',
-      module: 'viewAAUSuppliesCosts1MonthFlow',
-      project: '2'
+      module: 'Projects'
     });
-
   });
 
   client.on('response', (data) => {
     var query = data.query;
-    if (data.row) {
-    data.row.Tasks_start = new Date(data.row.Tasks_start);
-    data.row.Tasks_end = new Date(data.row.Tasks_end);
-    data.row.start = new Date(data.row.start);
-    data.row.end = new Date(data.row.end);
+    if (query == 'select' && data.module == 'viewAAUSuppliesCosts1MonthFlow') {
+      if (data.row) {
+        data.row.Tasks_start = new Date(data.row.Tasks_start);
+        data.row.Tasks_end = new Date(data.row.Tasks_end);
+        data.row.start = new Date(data.row.start);
+        data.row.end = new Date(data.row.end);
 
-    report.render(data.row);
+        report.render(data.row);
+      }
+    } else if (query == 'select' && data.module == 'Projects') {
+      window.projects.doselect(data.row);
+    } else {
+      console.log('not processed', data);
     }
   });
   window.client = client;
